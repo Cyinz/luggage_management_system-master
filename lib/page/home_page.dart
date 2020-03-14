dfc2716ba4aff88c7c19937243c29016ea4f9b8f
cyinz <13005684532@163.com>
2020-03-14 20:22:41 +0800

    轮播

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:luggagemanagementsystem/page/widget/swiperDiy.dart';
 import 'package:luggagemanagementsystem/provide/home_drawer.dart';
 import 'package:provide/provide.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
+  List _bannerList = [
+    "http://fdfs.xmcdn.com/group62/M06/16/15/wKgMZ1z3KxLQ2dbFAAHjQHda8Wc760.jpg",
+    "http://fdfs.xmcdn.com/group55/M0B/24/C0/wKgLdVxr18TyJ2nlAAIJVSk2i_o322.jpg",
+    "http://fdfs.xmcdn.com/group63/M0A/99/95/wKgMaFz_RD-BDyFjAAIO0iRtj0U176.jpg",
+    "http://fdfs.xmcdn.com/group61/M00/DD/05/wKgMZl0DDduydvLKAAHuFI7s-2U365.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 1080, height: 1980);
@@ -38,7 +46,7 @@ class HomePage extends StatelessWidget {
                 //  占满剩余空间
                 Expanded(child: Container()),
                 Container(
-                  padding: EdgeInsets.only(left: 15,right: 15),
+                  padding: EdgeInsets.only(left: 15, right: 15),
                   child: Divider(
                     height: 1,
                     color: Colors.grey[500],
@@ -57,13 +65,20 @@ class HomePage extends StatelessWidget {
           );
         },
       ),
-      body: Center(
-        child: Text("HomePage"),
+      body: RefreshIndicator(
+        child: ListView(
+          children: <Widget>[
+            SwiperDiy(swiperDataList: _bannerList),
+          ],
+        ),
+        onRefresh: _refresh,
       ),
     );
   }
 
-  //  加载行李员信息
+  //  轮播图
+
+  //  读取登陆行李员信息
   getClerk(BuildContext context) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     Provide.value<HomeDrawer>(context)
@@ -74,5 +89,10 @@ class HomePage extends StatelessWidget {
         .setClerkImg(sharedPreferences.getString('clerkImg'));
     Provide.value<HomeDrawer>(context)
         .setClerkHotel(sharedPreferences.getString('clerkHotel'));
+  }
+
+  //  下拉刷新方法
+  Future<Null> _refresh() async {
+    print("下拉刷新");
   }
 }
---
+++ lib/page/widget/swiperDiy.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+//  轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+
+  SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(1080),
+      height: ScreenUtil().setHeight(500),
+      child: Swiper(
+        itemCount: swiperDataList.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onTap: () {
+              Fluttertoast.showToast(
+                msg: "点了了${swiperDataList[index]}",
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+//                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage("${swiperDataList[index]}"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          );
+        },
+        autoplay: true,
+//        viewportFraction: 0.8,
+        scale: 0.8,
+        loop: false,
+        pagination: new SwiperPagination(
+          builder: DotSwiperPaginationBuilder(
+            color: Colors.white.withOpacity(0.5),
+            activeColor: Colors.white,
+          ),
+          margin: EdgeInsets.all(ScreenUtil().setWidth(10)),
+        ),
+      ),
+    );
+  }
+}
